52bee0ccceba56ba2d5d01f076f08f76ed518da5
Guilherme Bionde <gbionde@outlook.com>
2021-11-30 15:09:08 -0300

    Upload inicial do projeto.
    Adicionado todo o conteúdo.

---
+++ projeto/root/js/index.js
@@ -0,0 +1,7 @@
+function abrirSidebar() {
+    document.getElementById("idSidebar").style.width = "400px";
+}
+  
+function fecharSidebar() {
+    document.getElementById("idSidebar").style.width = "0";
+}
